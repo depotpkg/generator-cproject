ec8a77dcc8ee9133abe84420c61636f633bca0fc
skylerlee <skyler.ac.lee@gmail.com>
2025-03-22 08:37:13 +0800

    Fix bugs

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,23 +1,32 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(`Welcome to the first-rate ${chalk.red('generator-cproject')} generator!`));
-    return this.prompt([{
-      type: 'input',
-      name: 'projectName',
-      message: 'What is your project name?',
-      default: this.appname
-    }, {
-      type: 'input',
-      name: 'shortName',
-      message: 'What is the short name of your project?',
-      default: this.appname
-    }]).then(props => {
+    this.log(
+      yosay(
+        `Welcome to the first-rate ${chalk.red(
+          "generator-cproject"
+        )} generator!`
+      )
+    );
+    return this.prompt([
+      {
+        type: "input",
+        name: "projectName",
+        message: "What is your project name?",
+        default: this.appname
+      },
+      {
+        type: "input",
+        name: "shortName",
+        message: "What is the short name of your project?",
+        default: this.appname
+      }
+    ]).then(props => {
       this.props = props;
       this.props.shortPrefix = props.shortName.toUpperCase();
     });
@@ -25,13 +34,13 @@ module.exports = class extends Generator {
 
   writing() {
     this.fs.copyTpl(
-      this.templatePath('**/*'),
+      this.templatePath("**/*"),
       this.destinationPath(),
-      this.props
-    );
-    this.fs.copy(
-      this.templatePath('.*'),
-      this.destinationPath()
+      this.props,
+      {},
+      {
+        globOptions: { dot: true }
+      }
     );
   }
 };
